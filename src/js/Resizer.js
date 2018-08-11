461c33a0228b3250da894c343194de978b43af27
Timo Reitz <godsboss@gmx.de>
2018-08-11 04:10:54 +0200

    Let canvas size depend on window size.

---
+++ src/js/Resizer.js
@@ -0,0 +1,49 @@
+class Resizer {
+  constructor(window, size) {
+    this.window = window
+    this.size = size
+    this.listeners = []
+
+    // this.setMinimalFactor(1)
+    // this.setMargin(0, 0)
+  }
+
+  setMargin(horizontal, vertical) {
+    this.margin = {
+      horizontal: horizontal,
+      vertical: vertical
+    }
+    return this
+  }
+
+  setMinimalFactor(f) {
+    this.minimalFactor = f
+    return this
+  }
+
+  addListener(listener) {
+    if (this.listeners.length == 0) {
+      this.window.addEventListener('resize', () => this.resize(), false)
+    }
+    this.listeners.push(listener)
+    return this
+  }
+
+  resize() {
+    const hFactor = Math.max(1, Math.floor((this.window.innerWidth - this.margin.horizontal) / this.size.width))
+    const vFactor = Math.max(1, Math.floor((this.window.innerHeight - this.margin.vertical) / this.size.height))
+    const factor = Math.max(this.minimalFactor, Math.min(hFactor, vFactor))
+    this.listeners.forEach((listener) => listener(factor))
+  }
+}
+
+function canvasResizeListener(canvasElement) {
+  const initialWidth = canvasElement.width
+  const initialHeight = canvasElement.height
+  return function(factor) {
+    canvasElement.width = initialWidth * factor
+    canvasElement.height = initialHeight * factor
+  }
+}
+
+export { Resizer, canvasResizeListener }
--- src/js/init.js
+++ src/js/init.js
@@ -1,5 +1,6 @@
 import { Game } from './Game'
 import { Renderer } from './Renderer'
+import { Resizer, canvasResizeListener } from './Resizer'
 import { start as startRunner } from './Runner'
 
 window.addEventListener(
@@ -8,7 +9,18 @@ window.addEventListener(
   false
 )
 
+const WIDTH = 160
+const HEIGHT = 100
+
 function bootstrap(e) {
+  const canvas = document.getElementById('gfx')
+  canvas.width = WIDTH
+  canvas.height = HEIGHT
+  const resizer = (new Resizer(window, { width: WIDTH, height: HEIGHT })).
+    setMinimalFactor(2).
+    setMargin(10, 10).
+    addListener(canvasResizeListener(canvas))
+  resizer.resize()
   const img = new Image()
   img.addEventListener(
     'load',
